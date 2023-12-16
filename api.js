ab2a7975a1c094ebb373a3f5cdd1d590677af7f6
Sam <samuel.kinaitis@gmail.com>
2023-12-16 16:14:27 -0500

    Final

--- api.js
+++ api.js
@@ -1,203 +1,154 @@
-//Browser Geolocation API
-
-console.log("test");
-
-    // Function to calculate the distance between two points using Haversine formula
-    function calculateDistance(lat1, lon1, lat2, lon2) {
-        const R = 6371; // Radius of the Earth in kilometers
-        const dLat = (lat2 - lat1) * (Math.PI / 180);
-        const dLon = (lon2 - lon1) * (Math.PI / 180);
-        const a =
-          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-          Math.cos(lat1 * (Math.PI / 180)) *
-          Math.cos(lat2 * (Math.PI / 180)) *
-          Math.sin(dLon / 2) *
-          Math.sin(dLon / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const distance = R * c; // Distance in kilometers
-  
-        return distance;
-      }
-  
-      // Function to set a cookie with a name, value, and expiration in days
-      function setCookie(name, value, days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        var expires = "expires=" + date.toUTCString();
-        document.cookie = name + "=" + value + ";" + expires + ";path=/";
-      }
-  
-      // Function to retrieve the stored location from the cookie
-      function getUserStoredLocation() {
-        var name = "userLocation=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var cookies = decodedCookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-          var cookie = cookies[i].trim();
-          if (cookie.indexOf(name) == 0) {
-            return JSON.parse(cookie.substring(name.length, cookie.length));
-          }
-        }
-        return null;
-      }
-  
-      // Function to get the user's current location
-      function getUserLocations() {
-        // Retrieve the stored location
-        var storedLocation = getUserStoredLocation();
-  
-        if (storedLocation) {
-          var storedLatitude = storedLocation.latitude;
-          var storedLongitude = storedLocation.longitude;
-  
-          // Log the stored location
-          console.log("Stored Location:", storedLocation);
-  
-          // Get the current location
-          if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(function (currentPosition) {
-              var currentLatitude = currentPosition.coords.latitude;
-              var currentLongitude = currentPosition.coords.longitude;
-  
-              // Calculate the distance between the current and stored locations
-              var distance = calculateDistance(
-                currentLatitude,
-                currentLongitude,
-                storedLatitude,
-                storedLongitude
-              );
-  
-              console.log("Current Location:", { latitude: currentLatitude, longitude: currentLongitude });
-              console.log("Distance between current and stored locations:", distance.toFixed(2), "km");
-  
-              // You can set a threshold distance and compare it to determine if the locations are similar
-              var thresholdDistance = 1; // Adjust as needed
-              if (distance <= thresholdDistance) {
-                console.log("The locations are similar.");
-              } else {
-                console.log("The locations are different.");
-              }
+var map = L.map('map').setView([39.8283, -98.5795], 4); // Centered on the United States
+          
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              attribution: '© OpenStreetMap contributors'
+            }).addTo(map);
+          
+            var marker;
+          
+            // Event listener for map click
+            map.on('click', function(e) {
+              var clickedLatLng = e.latlng;
+              console.log("Clicked on: " + clickedLatLng.lat + ", " + clickedLatLng.lng);
+              findNearestTown(clickedLatLng);
             });
-          } else {
-            console.log("Geolocation is not available");
-          }
-        } else {
-          console.log("No stored location found.");
-        }
-      }
-  
-    //   // Event listener for the "Get Locations" button
-    //   document.getElementById("get-locations").addEventListener("click", function () {
-    //     getUserLocations();
-    //   });
-
-    function test(){
-
-      //Check if location is storaged (Check for how old last stored location is)
-
-
-          // Check if the Geolocation API is supported by the browser
-    if ('geolocation' in navigator) {
-      // Show loading message
-      showLoadingMessage();
-
-      // Use the Geolocation API to get the user's current position
-      navigator.geolocation.getCurrentPosition(
-          function (position) {
-              // Hide loading message
-              hideLoadingMessage();
-
-              // Success callback
-              const latitude = position.coords.latitude;
-              const longitude = position.coords.longitude;
-
-              console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-              
-              // Now you can use the obtained latitude and longitude for your purposes
-          },
-          function (error) {
-              // Hide loading message
-              hideLoadingMessage();
-
-              // Error callback
-              switch (error.code) {
-                  case error.PERMISSION_DENIED:
-                      console.error('User denied the request for Geolocation.');
-                      break;
-                  case error.POSITION_UNAVAILABLE:
-                      console.error('Location information is unavailable.');
-                      break;
-                  case error.TIMEOUT:
-                      console.error('The request to get user location timed out.');
-                      break;
-                  case error.UNKNOWN_ERROR:
-                      console.error('An unknown error occurred.');
-                      break;
+          
+            //input box enter
+            function handleKeyPress(event) {
+                // Check if the pressed key is Enter (keyCode 13)
+                if (event.keyCode === 13) {
+                  // Call your function here
+                  searchCity();
+                }
               }
-          }
-      );
-    } else {
-      // Geolocation is not supported by the browser
-      console.error('Geolocation is not supported by your browser.');
-    }
-
-    function showLoadingMessage() {
-      // You can implement your loading UI here, e.g., display a loading spinner or message
-      console.log('Getting current location, please stand by...');
-    }
-
-    function hideLoadingMessage() {
-      // You can implement logic to hide your loading UI here
-      console.log('Location request complete.');
-    }
-
-    }
-
-//NWS API
-
-//Get location weather information based off of 
-  function NWSLocationCall(latitude,longitude){
-      const apiUrl = `https://api.weather.gov/points/${latitude},${longitude}`;
-  //'ProgramName (email@adress)'
-  const userAgent = 'NWS-WeatherPortal (samuel.kinaitis@gmail.com)';
-  
-  fetch(apiUrl, {
-      headers: {
-          'User-Agent': userAgent
-      }
-  })
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
-          return response.json();
-      })
-      .then(data => {
-          // Extract the forecast URL
-          const forecastUrl = data.properties.forecast;
-  
-          // Make a request to the forecast URL to get the actual weather forecast data
-          fetch(forecastUrl, {
-              headers: {
-                  'User-Agent': userAgent
-              }
-          })
-              .then(forecastResponse => {
-                  if (!forecastResponse.ok) {
-                      throw new Error('Network response for forecast was not ok');
+          
+            function searchCity() {
+              var city = document.getElementById('city').value;
+          
+              if (!city) return;
+          
+              // Use OpenStreetMap Nominatim API to get coordinates
+              var nominatimUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${city},USA&bounded=1`;
+          
+              fetch(nominatimUrl)
+                .then(response => response.json())
+                .then(data => {
+                  if (data.length > 0) {
+                    // Display search results in a dropdown
+                    var resultsContainer = document.getElementById('results');
+                    resultsContainer.innerHTML = '';
+          
+                    var dropdown = document.createElement('select');
+                    dropdown.setAttribute('id', 'cityDropdown');
+                    dropdown.addEventListener('change', function() {
+                      var selectedCity = dropdown.options[dropdown.selectedIndex].text;
+                      displayCityOnMap(selectedCity);
+                    });
+          
+                    data.forEach(cityResult => {
+                      var option = document.createElement('option');
+                      option.value = cityResult.display_name;
+                      option.text = cityResult.display_name;
+                      dropdown.appendChild(option);
+                    });
+          
+                    resultsContainer.appendChild(dropdown);
+          
+                    // Display the first location on the map by default
+                    displayCityOnMap(data[0].display_name);
+                  } else {
+                    console.log("No results found for " + city);
                   }
-                  return forecastResponse.json();
-              })
-              .then(forecastData => {
-                  // Handle the forecast data
-                  console.log(forecastData);
-              })
-              .catch(forecastError => {
-                  console.error('Error fetching forecast data:', forecastError);
-              });
-      })
-      .catch(error => {
-          console.error('Error fetching weather data:', error);
-      });
+                })
+                .catch(error => console.error('Error:', error));
+            }
+          
+            function displayCityOnMap(selectedCity) {
+              // Use OpenStreetMap Nominatim API to get coordinates for the selected city
+              var nominatimUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${selectedCity}&bounded=1`;
+          
+              fetch(nominatimUrl)
+                .then(response => response.json())
+                .then(data => {
+                  if (data.length > 0) {
+                    var coordinates = data[0];
+                    var latlng = L.latLng(coordinates.lat, coordinates.lon);
+                    map.flyTo(latlng, 10);
+          
+                    if (marker) {
+                      map.removeLayer(marker);
+                    }
+          
+                    marker = L.marker(latlng).addTo(map);
+                    console.log("Coordinates for " + selectedCity + ": " + coordinates.lat + ", " + coordinates.lon);
+                    showWeather(coordinates.lat, coordinates.lon);
+                  } else {
+                    console.log("Coordinates not found for " + selectedCity);
+                  }
+                })
+                .catch(error => console.error('Error:', error));
+            }
+          
+            function findNearestTown(clickedLatLng) {
+              // Use OpenStreetMap Nominatim API to find the nearest town
+              var nominatimUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${clickedLatLng.lat}&lon=${clickedLatLng.lng}&zoom=10&addressdetails=1,USA&bounded=1`;
+          
+              fetch(nominatimUrl)
+                .then(response => response.json())
+                .then(data => {
+                  var town = data.address.town || data.address.city || data.address.village || data.address.hamlet;
+                  var state = data.address.state;
+                  if (town && state) {
+                    console.log("Nearest town: " + town + ", " + state);
+                    displayCityOnMap(`${town}, ${state}`);
+                  } else {
+                    console.log("No town found near the clicked location");
+                  }
+                })
+                .catch(error => console.error('Error:', error));
+            }
+
+// Function to get user's location
+function getUserLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      // Call the showWeather function with coordinates
+      showWeather(position.coords.latitude, position.coords.longitude);
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
   }
-  
+}
+
+// Function to show weather information based on coordinates
+function showWeather(latitude, longitude) {
+  // Call National Weather Service API to get weather information
+  const weatherApiUrl = `https://api.weather.gov/points/${latitude},${longitude}`;
+
+  // Use Fetch API for Ajax request
+  fetch(weatherApiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const forecastUrl = data.properties.forecast;
+      
+      // Another Fetch request for forecast data
+      return fetch(forecastUrl);
+    })
+    .then(response => response.json())
+    .then(forecastData => {
+      // Display weather information for each forecast period
+      const forecastPeriods = forecastData.properties.periods;
+
+      let weatherInfo = '<h2>Weather Information</h2>';
+
+      forecastPeriods.forEach(period => {
+        weatherInfo += `<p><strong>${period.name}:</strong> ${period.detailedForecast}</p>`;
+      });
+
+      document.getElementById('weather-info').innerHTML = weatherInfo;
+      document.getElementById("weather-info").scrollIntoView({ behavior: "smooth" });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
